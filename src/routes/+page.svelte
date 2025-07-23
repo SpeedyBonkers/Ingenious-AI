94fcaa9a08f2a9974cc2b29ac323bc11ad6951aa
SpeedyBonkers <deine@email.deleonboonfoe@gmail.com>
2025-07-23 11:04:36 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+
+
+<script>
+function click(){
+  alert("404")
+}
+</script>
+
+
+<main>
+
+    <div class="clickMe">
+      
+    </div>
+    <button class="juli" on:click={click}>click</button>
+
+
+      <div class="test">test test</div>
+
+      <div class="moin">example</div>
+    <div>baum</div>  
+    </div>
+</main>
+
+
+<style>
+main{
+  background: #ffffff;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;   
+  justify-content: space-around; 
+  padding: 0.4%;
+  box-sizing: border-box;  
+}
+
+.hallo {
+  height: 20%;
+  width: 50%;
+  background-color:aqua;
+  align-items: center;
+  justify-content: space-around;
+  display: flex;
+}
+
+.moin{
+  background-color: #de1717;
+  justify-content: center;
+  align-items: center;
+  height: 30%;
+  width: 20%;
+  display: flex;
+}
+    
+.juli{
+  height: 10%;
+  justify-content: center;
+}
+
+</style>
---
+++ src/routes/story/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+
+<main>
+
+    <div class="mainComponent">
+        
+    <button>
+        <a href="/">Back</a>
+    </button>
+    </div>
+    
+
+
+</main>
+
+
+<style>
+    main{
+  background: #050505;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;   
+  justify-content: center; 
+  padding: 0.4%;
+  box-sizing: border-box;  
+}
+
+.mainComponent {
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+}
+
+</style>
